45f120df075122b622b69e8cc593d46d06ed6079
DineshReddy <ndkr2716@gmail.com>
2022-12-01 19:53:25 +0530

    Assignment Completed

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Welcome(){
+  return(
+    <div className='App'>
+      <h1>About Us</h1>
+      <p className='p1'>At Zappos.com, our purpose is simple: to live and deliver WOW</p>
+      <p className='p2'>Twenty years ago, we began as a small online retailer that only sold shoes. Today, we still sell shoes ---- as well as clothing, handbags, accessories, and more. That "more" is providing the very best customer service, customer experience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver happiness to customers, employees, vendors, shareholders, and the community in a long-term sustainable way.</p>
+      <p className='p3'>We hope that in the future people won't even realize we started selling shoes online. Instead they will know that Zappos as a service company that just happens to sell _______</p>
+      <img src='https://geekster-in.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3f668fa5-ba79-40cd-afc1-5a3c649f27ae%2FUntitled.png?table=block&id=0e74f2f2-2963-41d0-94f7-b66795e4e0bb&spaceId=d21b1a65-81c4-4c29-a7ac-4ece53e36277&width=1440&userId=&cache=v2' alt='air-bnb' className='image'></img>
+    </div>
+  )
+}
+
+export default Welcome;
+
+
+
+
+
